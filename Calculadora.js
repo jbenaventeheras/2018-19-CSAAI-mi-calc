3f68873339611d34440f83365a4fc2560f5e127f
jbenaventeheras <javierbenavente@MacBook.local>
2019-03-17 17:52:52 +0100

    js terminado

---
+++ Calculadora.js
@@ -0,0 +1,92 @@
+function main() {
+
+  var num1 = 0
+  var num2 = 0
+  var res = 0
+  var operador= ""
+
+
+  var display = document.getElementById("Pantalla");
+  var clean = document.getElementById("CE")
+  var del = document.getElementById("C");
+  var suma = document.getElementById("+");
+  var resta = document.getElementById("-");
+  var multiplicacion = document.getElementById("*");
+  var division = document.getElementById("/");
+  var igual = document.getElementById("=");
+  var uno = document.getElementById("1");
+  var dos = document.getElementById("2");
+  var tres = document.getElementById("3");
+  var cuatro = document.getElementById("4");
+  var cinco = document.getElementById("5");
+  var seis = document.getElementById("6");
+  var siete = document.getElementById("7");
+  var ocho = document.getElementById("8");
+  var nueve = document.getElementById("9");
+  var cero = document.getElementById("0");
+  var pi = document.getElementById("pi");
+
+
+
+
+  cero.onclick = () => {display.innerHTML = display.innerHTML + "0"}
+  uno.onclick = () => {display.innerHTML = display.innerHTML + "1"}
+  dos.onclick = () => {display.innerHTML = display.innerHTML + "2"}
+  tres.onclick = () => {display.innerHTML = display.innerHTML + "3"}
+  cuatro.onclick = () => {display.innerHTML = display.innerHTML + "4"}
+  cinco.onclick = () => {display.innerHTML = display.innerHTML + "5"}
+  seis.onclick = () => {display.innerHTML = display.innerHTML + "6"}
+  siete.onclick = () => {display.innerHTML = display.innerHTML + "7"}
+  ocho.onclick = () => {display.innerHTML = display.innerHTML + "8"}
+  nueve.onclick = () => {display.innerHTML = display.innerHTML + "9"}
+  pi.onclick = () => {display.innerHTML = display.innerHTML + "3.14159265359"}
+
+
+  suma.onclick = () => {num1 = display.innerHTML
+    display.innerHTML = display.innerHTML + "+";   operador = "+"}
+
+
+  multiplicacion.onclick = () => {num1= display.innerHTML
+    display.innerHTML = display.innerHTML + "*" ;operador = "*"}
+
+  division.onclick = () => {num1 = display.innerHTML
+    display.innerHTML = display.innerHTML + "/" ;operador = "/"}
+
+  resta.onclick = () => {
+    num1 = display.innerHTML
+    display.innerHTML = display.innerHTML + "-" ;operador = "-"}
+
+  clean.onclick = () => {display.innerHTML = display.innerHTML.slice(0,-1)}
+
+  del.onclick = () => {display.innerHTML = ""}
+
+
+
+
+
+  igual.onclick = () => {
+      linea = display.innerHTML;
+
+      switch(operador){
+          case "+":
+            num2 = linea.split("+")[1];
+            res = parseFloat(num1) + parseFloat(num2);
+            break;
+          case "-":
+          num2 = linea.split("-")[1];
+            res = parseFloat(num1) - parseFloat(num2);
+            break;
+          case "*":
+            num2 = linea.split("*")[1];
+            res = parseFloat(num1) * parseFloat(num2);
+            break;
+          case "/":
+          num2 = linea.split("/")[1];
+            res = parseFloat(num1) / parseFloat(num2);
+            break;
+        }
+
+      display.innerHTML = res}
+
+
+  }
